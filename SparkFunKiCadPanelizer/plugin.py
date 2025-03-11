cb0a73f2ea8a6e32e9e6803f0323b30f64005c14
PaulZC <pc235603@gmail.com>
2025-03-11 17:58:50 +0000

    Add checks for version 9. Allow panelizer to run if version check fails

--- SparkFunKiCadPanelizer/plugin.py
+++ SparkFunKiCadPanelizer/plugin.py
@@ -27,7 +27,7 @@ class PanelizerPlugin(pcbnew.ActionPlugin, object):
         
         self._pcbnew_frame = None
 
-        self.supportedVersions = ['7.','8.']
+        self.supportedVersions = ['7.','8.','9.']
 
         self.kicad_build_version = pcbnew.GetBuildVersion()
 
@@ -96,84 +96,84 @@ class PanelizerPlugin(pcbnew.ActionPlugin, object):
         def run_panelizer(dlg, p_panelizer):
             self.logger.log(logging.INFO, "Running Panelizer")
 
-            if self.IsSupported():
-                command = []
+            if not self.IsSupported():
+                # Log an error if this version of KiCad has not been tested
+                self.logger.log(logging.ERROR, "Version check failed. \"{}\" may not be supported. Panelizing may fail".format(self.kicad_build_version))
 
-                convertDimensions = 1.0
-                if dlg.CurrentSettings()["dimensionsInchesBtn"]:
-                    convertDimensions = 25.4
-                
-                panelx = float(dlg.CurrentSettings()["panelSizeXCtrl"]) * convertDimensions
-                panely = float(dlg.CurrentSettings()["panelSizeYCtrl"]) * convertDimensions
-                command.extend(['--panelx','{:.6f}'.format(panelx)])
-                command.extend(['--panely','{:.6f}'.format(panely)])
+            command = []
 
-                smallerThan = dlg.CurrentSettings()["panelSizeSmallerBtn"]
-                if smallerThan:
-                    command.append('--smaller')
-                else:
-                    command.append('--larger')
+            convertDimensions = 1.0
+            if dlg.CurrentSettings()["dimensionsInchesBtn"]:
+                convertDimensions = 25.4
+            
+            panelx = float(dlg.CurrentSettings()["panelSizeXCtrl"]) * convertDimensions
+            panely = float(dlg.CurrentSettings()["panelSizeYCtrl"]) * convertDimensions
+            command.extend(['--panelx','{:.6f}'.format(panelx)])
+            command.extend(['--panely','{:.6f}'.format(panely)])
 
-                vscorelayer = dlg.CurrentSettings()[dlg.vscore_layer]
-                command.extend(['--vscorelayer', vscorelayer, '--vscoretextlayer', vscorelayer])
-
-                gapx = float(dlg.CurrentSettings()["gapsVerticalCtrl"]) * convertDimensions
-                gapy = float(dlg.CurrentSettings()["gapsHorizontalCtrl"]) * convertDimensions
-                command.extend(['--gapx','{:.6f}'.format(gapx)])
-                command.extend(['--gapy','{:.6f}'.format(gapy)])
-
-                removeRight = dlg.CurrentSettings()["removeRightVerticalCheck"]
-                if removeRight:
-                    command.append('--norightgap')
-
-                exposeedge = dlg.CurrentSettings()["productionExposeCheck"]
-                if exposeedge:
-                    command.append('--exposeedge')
-
-                fiducials = dlg.CurrentSettings()["productionBordersCheck"]
-                leftright = dlg.CurrentSettings()["productionFiducialsCheck"]
-                if not exposeedge:
-                    if fiducials:
-                        # Default the rail width to 1/4" and nudge by 1/4 of the rail width.
-                        # This provides the clearance needed for clamping and AOI Inspection of the fiducials.
-                        # This is nasty. The default should be in panelizer.py. But I can't think of a solution
-                        # which is good for everyone - including anyone running the panelizer from the command line.
-                        command.extend(['--hrail','6.35','--vrail','6.35'])
-                        command.extend(['--fiducialpos','0.25'])
-                        if leftright:
-                            command.append('--fiducialslr')
-                        else:
-                            command.append('--fiducialstb')
-                else:
-                    if fiducials:
-                        # Same comment as above
-                        command.extend(['--vrail','6.35'])
-                        command.extend(['--fiducialpos','0.25'])
-                        command.append('--fiducialslr')
-
-                self.logger.log(logging.INFO, command)
-
-                board = pcbnew.GetBoard()
-
-                if board is not None:
-                    sysExit, report = p_panelizer.startPanelizerCommand(command, board, self.ordering_instructions, self.logger)
-                    logWarn = logging.INFO
-                    if sysExit >= 1:
-                        logWarn = logging.WARN
-                    if sysExit >= 2:
-                        logWarn = logging.ERROR
-                    self.logger.log(logWarn, report)
-                    if sysExit > 0:
-                        wx.MessageBox("Panelizer " + ("warning" if (sysExit == 1) else "error") + ".\nPlease check panelizer.log for details.",
-                            ("Warning" if (sysExit == 1) else "Error"), wx.OK | (wx.ICON_WARNING if (sysExit == 1) else wx.ICON_ERROR))
-                    else:
-                        wx.MessageBox("Panelizer complete.\nPlease check panelizer.log for details.",
-                            "Info", wx.OK | wx.ICON_INFORMATION)
-                else:
-                    self.logger.log(logging.ERROR, "Could not get the board")
-
+            smallerThan = dlg.CurrentSettings()["panelSizeSmallerBtn"]
+            if smallerThan:
+                command.append('--smaller')
             else:
-                self.logger.log(logging.ERROR, "Version check failed. \"{}\" not supported".format(self.kicad_build_version))
+                command.append('--larger')
+
+            vscorelayer = dlg.CurrentSettings()[dlg.vscore_layer]
+            command.extend(['--vscorelayer', vscorelayer, '--vscoretextlayer', vscorelayer])
+
+            gapx = float(dlg.CurrentSettings()["gapsVerticalCtrl"]) * convertDimensions
+            gapy = float(dlg.CurrentSettings()["gapsHorizontalCtrl"]) * convertDimensions
+            command.extend(['--gapx','{:.6f}'.format(gapx)])
+            command.extend(['--gapy','{:.6f}'.format(gapy)])
+
+            removeRight = dlg.CurrentSettings()["removeRightVerticalCheck"]
+            if removeRight:
+                command.append('--norightgap')
+
+            exposeedge = dlg.CurrentSettings()["productionExposeCheck"]
+            if exposeedge:
+                command.append('--exposeedge')
+
+            fiducials = dlg.CurrentSettings()["productionBordersCheck"]
+            leftright = dlg.CurrentSettings()["productionFiducialsCheck"]
+            if not exposeedge:
+                if fiducials:
+                    # Default the rail width to 1/4" and nudge by 1/4 of the rail width.
+                    # This provides the clearance needed for clamping and AOI Inspection of the fiducials.
+                    # This is nasty. The default should be in panelizer.py. But I can't think of a solution
+                    # which is good for everyone - including anyone running the panelizer from the command line.
+                    command.extend(['--hrail','6.35','--vrail','6.35'])
+                    command.extend(['--fiducialpos','0.25'])
+                    if leftright:
+                        command.append('--fiducialslr')
+                    else:
+                        command.append('--fiducialstb')
+            else:
+                if fiducials:
+                    # Same comment as above
+                    command.extend(['--vrail','6.35'])
+                    command.extend(['--fiducialpos','0.25'])
+                    command.append('--fiducialslr')
+
+            self.logger.log(logging.INFO, command)
+
+            board = pcbnew.GetBoard()
+
+            if board is not None:
+                sysExit, report = p_panelizer.startPanelizerCommand(command, board, self.ordering_instructions, self.logger)
+                logWarn = logging.INFO
+                if sysExit >= 1:
+                    logWarn = logging.WARN
+                if sysExit >= 2:
+                    logWarn = logging.ERROR
+                self.logger.log(logWarn, report)
+                if sysExit > 0:
+                    wx.MessageBox("Panelizer " + ("warning" if (sysExit == 1) else "error") + ".\nPlease check panelizer.log for details.",
+                        ("Warning" if (sysExit == 1) else "Error"), wx.OK | (wx.ICON_WARNING if (sysExit == 1) else wx.ICON_ERROR))
+                else:
+                    wx.MessageBox("Panelizer complete.\nPlease check panelizer.log for details.",
+                        "Info", wx.OK | wx.ICON_INFORMATION)
+            else:
+                self.logger.log(logging.ERROR, "Could not get the board")
 
             dlg.GetParent().EndModal(wx.ID_OK)
 
